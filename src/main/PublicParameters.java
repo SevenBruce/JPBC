f72e0405dafda5fefb16227e1eb0c1e9f39d04f6
SevenBruce <646809863@qq.com>
2017-11-29 12:20:00 +0100

    Update PublicParameters.java

--- src/main/PublicParameters.java
+++ src/main/PublicParameters.java
@@ -3,7 +3,7 @@ package main;
 public class PublicParameters {
 	
 	
-	public final static int COUNT = 100;
+	public final static int COUNT = 5000;
 	
 	public static int LIMIT = 100000;
 	public static int LEAPES = 316;   /* = square root of LIMIT */
